75724432fbaa78830982f274d446c11c41d81206
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-02-04 18:52:48 +0300

    add train

---
+++ snake_game_dqn/train.py
@@ -0,0 +1,57 @@
+import pygame
+import matplotlib.pyplot as plt
+from agent import Agent
+from game import SnakeGame
+
+
+def plot(scores, mean_scores):
+    plt.clf()
+    plt.title('Training...')
+    plt.xlabel('Number of Games')
+    plt.ylabel('Score')
+    plt.plot(scores, label='Score per Game')
+    plt.plot(mean_scores, label='Mean Score')
+    plt.legend()
+    plt.pause(0.001)
+
+
+def train():
+    pygame.init()
+    agent = Agent()
+    game = SnakeGame(render=False)
+    scores = []
+    mean_scores = []
+    total_score = 0
+    record = 0
+
+    while True:
+        game.reset()
+        state_old = agent.get_state(game)
+        done = False
+        score = 0
+
+        while not done:
+            action = agent.get_action(state_old)
+            reward, done, score = game.play_step(action)
+            state_new = agent.get_state(game)
+            agent.train_short_memory(state_old, action, reward, state_new, done)
+            agent.remember(state_old, action, reward, state_new, done)
+            state_old = state_new
+
+        agent.n_games += 1
+        agent.train_long_memory()
+
+        if score > record:
+            record = score
+            agent.model.save("model_dqn.pth")
+
+        print("Game", agent.n_games, "Score", score, "Record:", record)
+        scores.append(score)
+        total_score += score
+        mean_score = total_score / agent.n_games
+        mean_scores.append(mean_score)
+        plot(scores, mean_scores)
+
+
+if __name__ == "__main__":
+    train()
