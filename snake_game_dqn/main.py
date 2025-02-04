0c59ea8ad0b817e1fe49756c90c683f6d40030f5
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-02-04 19:08:52 +0300

    load model

--- snake_game_dqn/main.py
+++ snake_game_dqn/main.py
@@ -7,8 +7,11 @@ def run():
     pygame.init()
     game = SnakeGame(render=True)
     agent = Agent()
+
+    # Now that the load method exists, we can do:
     agent.model.load("model_dqn.pth")
     agent.epsilon = 0  # Disable exploration for evaluation
+
     while True:
         game.reset()
         state = agent.get_state(game)
